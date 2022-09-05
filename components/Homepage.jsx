ca5f11d18bc4584f16253858a2bea5282eb87cd6
YeastWrangler <102862775+YeastWrangler@users.noreply.github.com>
2022-09-05 16:08:14 +0100

    Testing Homepage component

---
+++ components/Homepage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+
+const Homepage = () => {
+    return(
+  <View>
+    <Text>Testing here</Text>
+
+    </View>)
+}
+
+const styles = StyleSheet.create({
+    backGround: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    }
+})
+
+export default Homepage
