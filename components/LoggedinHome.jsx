93b0b9ace1c5af5967286ff339664972fd28d61c
YeastWrangler <102862775+YeastWrangler@users.noreply.github.com>
2022-09-09 12:22:03 +0100

    added PostPets screen with input fields and picker

--- components/LoggedinHome.jsx
+++ components/LoggedinHome.jsx
@@ -10,7 +10,6 @@ import {
 } from "react-native";
 
 const LoggedinHome = ({ navigation }) => {
-  const userName = "Shrek";
 
   const handlePetsPress = () => {
     navigation.navigate("PetsList");
@@ -22,12 +21,11 @@ const LoggedinHome = ({ navigation }) => {
   };
 
   const handlePostServices = () => {
-    navigation.navigate("");
+    navigation.navigate("PostServices");
   };
 
-
   const handlePostPet = () => {
-    navigation.navigate("");
+    navigation.navigate("PostPets");
   };
 
   return (
@@ -66,7 +64,7 @@ const LoggedinHome = ({ navigation }) => {
       </View>
       <View style={styles.secondButtonContainer}>
         <TouchableOpacity style={styles.secondButton} onPress={handlePostPet}>
-          <Text style={styles.secondButtonText}>Post for Pet Help</Text>
+          <Text style={styles.secondButtonText}>Needing Pet Help?</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.secondButtonContainer}>
@@ -74,7 +72,7 @@ const LoggedinHome = ({ navigation }) => {
           style={styles.secondButton}
           onPress={handlePostServices}
         >
-          <Text style={styles.secondButtonText}>Post for Pet Services</Text>
+          <Text style={styles.secondButtonText}>Offering Services?</Text>
         </TouchableOpacity>
       </View>
       <Button title="postings" />
---
+++ components/PostServices.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, } from "react-native";
+
+const PostServices = () => {
+  return (
+    <Text>PostServices</Text>
+  )
+}
+
+export default PostServices
