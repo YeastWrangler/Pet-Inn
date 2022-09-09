6683133ca3f29a9fa9eec2b06369bdeb534664ee
aludford <andrewludford@hotmail.co.uk>
2022-09-09 15:32:32 +0100

    add code for individual sitter listing

--- components/IndividualSitterListing.jsx
+++ components/IndividualSitterListing.jsx
@@ -9,81 +9,72 @@ import {
   Pressable,
 } from "react-native";
 import { sitterListingTestData } from "../test data/sitterListingTestData";
-import Slideshow from "react-native-image-slider-show";
 
 const IndividualSitterListing = ({ navigation, route }) => {
   const { id } = route.params;
   // console.log(id);
   const testData = sitterListingTestData;
 
-  const filteredData = testData.filter((element) => element._id === id);
+  const [filteredData] = testData.filter((element) => element._id === id);
 
   return (
     <ScrollView>
       <View style={styles.container}>
-        <Text>reached individual sitter page</Text>
-        {/* <Text style={styles.listingTitle}>{filteredData[0].title}</Text>
-          <Text style={styles.content}>{filteredData[0].location}</Text>
-          <View style={styles.datesContainer}>
-            <View style={styles.infoContainer}>
-              <Text style={styles.heading}>Date From: </Text>
-              <Text style={styles.content}>{filteredData[0].dates.from}</Text>
-            </View>
-            <View style={styles.infoContainer}>
-              <Text style={styles.heading}>Date To: </Text>
-              <Text style={styles.content}>{filteredData[0].dates.to}</Text>
-            </View>
-          </View>
-  
-  
+        <Text style={styles.listingTitle}>{filteredData.title}</Text>
+        <Text style={styles.content}>{filteredData.location}</Text>
+        <View style={styles.datesContainer}>
           <View style={styles.infoContainer}>
-            <Text style={styles.heading}>Pet(s): </Text>
-            <Text style={styles.content}>
-              {filteredData[0].pets.map((pet, index) => {
-                if (index === filteredData[0].pets.length - 1) {
-                  return pet;
-                } else {
-                  return `${pet}, `;
-                }
-              })}
-            </Text>
+            <Text style={styles.heading}>Date From: </Text>
+            <Text style={styles.content}>{filteredData.dates.from}</Text>
           </View>
-  
-          <Slideshow
-            height={200}
-            dataSource={filteredData[0].image_urls.map((photo) => {
-              return { url: photo };
+          <View style={styles.infoContainer}>
+            <Text style={styles.heading}>Date To: </Text>
+            <Text style={styles.content}>{filteredData.dates.to}</Text>
+          </View>
+        </View>
+
+        <View style={styles.infoContainer}>
+          <Text style={styles.heading}>Pet(s) covered: </Text>
+          <Text style={styles.content}>
+            {filteredData.suitable_pets.map((pet, index) => {
+              if (index === filteredData.suitable_pets.length - 1) {
+                return pet;
+              } else {
+                return `${pet}, `;
+              }
             })}
-          />
-          <Text style={styles.payment}>
-            {filteredData[0].payment === 0
-              ? "Free"
-              : `£${filteredData[0].payment}`}
           </Text>
-          <View style={styles.infoContainer}>
-            <Text style={styles.heading}>Additional Info: </Text>
-            <Text style={styles.content}>{filteredData[0].additional_info}</Text>
-          </View>
-  
-          <View style={styles.infoContainer}>
-            <Text style={styles.heading}>Date Added: </Text>
-            <Text style={styles.content}>{filteredData[0].date_added}</Text>
-          </View>
-  
-          <View style={styles.infoContainer}>
-            <Text style={styles.heading}>Posted By: </Text>
-            <Text style={styles.content}>{filteredData[0].username}</Text>
-          </View>
-  
-          <Pressable
-            style={styles.contactButton}
-            onPress={() => {
-              console.log("open chat clicked");
-              //needs to open chat window/page when clicked
-            }}
-          >
-            <Text style={styles.contactButtonText}>Contact Owner</Text>
-          </Pressable> */}
+        </View>
+
+        <Text style={styles.payment}>
+          {filteredData.payment === 0 ? "Free" : `£${filteredData.payment}`}
+        </Text>
+
+        <View style={styles.infoContainer}>
+          <Text style={styles.heading}>Date Posted: </Text>
+          <Text style={styles.content}>{filteredData.data_posted}</Text>
+        </View>
+
+        <View style={styles.infoContainer}>
+          <Text style={styles.heading}>Posted By: </Text>
+          <Text style={styles.content}>{filteredData.username}</Text>
+        </View>
+
+        <View style={styles.infoContainer}>
+          <Text style={styles.heading}>Rating: </Text>
+          <Text style={styles.content}>Insert Rating</Text>
+          {/* need to insert rating */}
+        </View>
+
+        <Pressable
+          style={styles.contactButton}
+          onPress={() => {
+            console.log("open chat clicked");
+            //needs to open chat window/page when clicked
+          }}
+        >
+          <Text style={styles.contactButtonText}>Contact Service Provider</Text>
+        </Pressable>
       </View>
     </ScrollView>
   );
