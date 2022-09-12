150ad5058c720cca5d2ffdeedca0c08066499c94
aludford <andrewludford@hotmail.co.uk>
2022-09-12 14:36:45 +0100

    connect owner listing page to BE

--- components/PetsList.jsx
+++ components/PetsList.jsx
@@ -8,86 +8,21 @@ import {
   Pressable,
 } from "react-native";
 import { colors } from "../assets/colors";
-import { OwnerListingTestData } from "../test data/OwnerListingTestData";
+import { getOwnerListings } from "../dbCalls/ownerListing";
 
 const PetsList = ({ navigation }) => {
-  // const [ownerListings, setOwnerListings] = useState([]);
+  const [ownerListings, setOwnerListings] = useState([]);
 
-  // useEffect(() => {
-  //
-  // }, []);
-
-  const data = OwnerListingTestData;
-
-  // const data = [
-  //   {
-  //     key: "1", // FlatList must have a key property. see workaround below
-  //     location: "Birmingham",
-  //     images: [
-  //       "https://kb.rspca.org.au/wp-content/uploads/2018/11/golder-retriever-puppy.jpeg",
-  //     ],
-  //     petType: "dog",
-  //     other: "must take dog for 2 walks every day",
-  //   },
-  //   {
-  //     key: "2",
-  //     location: "London",
-  //     images: [
-  //       "https://cdn.reptileadvisor.com/wp-content/uploads/2020/03/plants-for-snake-tank.jpg",
-  //     ],
-  //     petType: "snake",
-  //     other: "feed snake once per week",
-  //   },
-  //   {
-  //     key: "3",
-  //     location: "Manchester",
-  //     images: [
-  //       "https://miniblogcore.azurewebsites.net/Posts/files/cat_637121647509154829.jpg",
-  //     ],
-  //     petType: "cat",
-  //     other: "dont let cat out at night",
-  //   },
-  //   {
-  //     key: "4",
-  //     location: "Bristol",
-  //     images: [
-  //       "https://miniblogcore.azurewebsites.net/Posts/files/cat_637121647509154829.jpg",
-  //     ],
-  //     petType: "cat",
-  //     other:
-  //       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, nulla! Porro iste quidem voluptatem harum officiis, ratione consectetur quos reiciendis!",
-  //   },
-  //   {
-  //     key: "5",
-  //     location: "Leeds",
-  //     images: [
-  //       "https://miniblogcore.azurewebsites.net/Posts/files/cat_637121647509154829.jpg",
-  //     ],
-  //     petType: "rabbit",
-  //     other:
-  //       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, nulla! Porro iste quidem voluptatem harum officiis, ratione consectetur quos reiciendis!",
-  //   },
-  //   {
-  //     key: "6",
-  //     location: "Glasgow",
-  //     images: [
-  //       "https://miniblogcore.azurewebsites.net/Posts/files/cat_637121647509154829.jpg",
-  //     ],
-  //     petType: "Dog",
-  //     other:
-  //       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, officia!",
-  //   },
-  //   {
-  //     key: "7",
-  //     location: "London",
-  //     images: [
-  //       "https://miniblogcore.azurewebsites.net/Posts/files/cat_637121647509154829.jpg",
-  //     ],
-  //     petType: "Cat",
-  //     other:
-  //       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur numquam consectetur explicabo ipsam deleniti laborum rem ipsum, delectus ratione! In quasi optio explicabo aperiam ex error cum, reprehenderit impedit nesciunt at temporibus! Hic eius tenetur incidunt quas quibusdam veritatis fuga? Nulla, odio consectetur inventore recusandae corporis reiciendis sint at architecto ut quibusdam iste culpa nihil ipsam modi voluptas dolorum aliquid? Inventore, a minus cum nostrum esse sint quis cupiditate architecto reprehenderit molestiae quam error repellat tempora eligendi fuga obcaecati itaque explicabo repellendus. Minima maxime voluptatem nobis libero porro facilis dignissimos esse! Laboriosam natus adipisci hic rem repellendus commodi nihil sunt, dolorum cumque cupiditate nemo, rerum dolores consectetur non esse architecto id tenetur aliquam velit itaque provident at. Eum aliquam rerum voluptas? Vitae ducimus neque quisquam consequatur consectetur, doloribus dolorem soluta incidunt reprehenderit exercitationem rerum maiores nesciunt iste consequuntur cupiditate temporibus nam asperiores cum voluptas corrupti, reiciendis debitis. Alias recusandae cumque, incidunt placeat nisi quod earum perspiciatis ratione magnam ut facere id dignissimos veniam quo enim eaque aperiam dolore, rerum expedita! Iure voluptas natus, nisi ipsam ad, exercitationem ab blanditiis sapiente rerum architecto voluptatum recusandae doloremque eaque dicta, impedit iste eos omnis est temporibus excepturi illum nemo saepe! Impedit, vitae! Fugit consectetur odit, laborum deserunt officia sit adipisci magni illum culpa temporibus numquam explicabo et repellendus distinctio veniam animi tempora molestiae porro eaque maiores veritatis quasi. Facere sint eius eveniet deserunt? Veritatis repellat exercitationem quaerat omnis id quasi inventore vero saepe excepturi incidunt dolor, ab reiciendis? Aspernatur, reprehenderit! Facere, magni cumque. Reprehenderit enim voluptatibus aliquid saepe qui quos veniam. Magnam, necessitatibus recusandae sed voluptates suscipit molestias pariatur reprehenderit voluptatibus, ab quia sint voluptas modi possimus illo officia consequatur nam labore similique cupiditate maiores dolorum eveniet dolore. Odio maxime nam hic, veniam, corporis deleniti, sunt ducimus quis nihil laborum facilis excepturi consectetur?",
-  //   },
-  // ];
+  useEffect(() => {
+    getOwnerListings()
+      .then(({ ownerListing }) => {
+        setOwnerListings(ownerListing);
+      })
+      .catch((err) => {
+        console.log(err);
+        //need to error handle
+      });
+  }, []);
 
   return (
     <View>
@@ -97,11 +32,8 @@ const PetsList = ({ navigation }) => {
 
       <FlatList
         contentContainerStyle={{ paddingBottom: 50 }}
-        keyExtractor={(item) =>
-          item._id
-        } /*----> change'id' for a unique id from our data table.
-        This tell Flat list not to look for key property but for our specified property instead */
-        data={OwnerListingTestData}
+        keyExtractor={(item) => item._id}
+        data={ownerListings}
         //need to destructure item as FlatList has created an object with multiple keys
         renderItem={({ item }) => {
           return (
@@ -160,7 +92,6 @@ const styles = StyleSheet.create({
     padding: 16,
     fontSize: 16,
     alignItems: "center",
-    // paddingBottom: 50,
   },
   centerContent: {
     alignItems: "center",
--- dbCalls/ownerListing.js
+++ dbCalls/ownerListing.js
@@ -1,101 +1,15 @@
-async function postOwnerListing(newPost) {
-	const { MongoClient } = require("mongodb");
-	const uri =
-		"mongodb+srv://tedNorthcoders:PetInn0369@petinn.gyiq5ap.mongodb.net/?retryWrites=true&w=majority";
-	const client = new MongoClient(uri);
+import axios from "axios";
 
-	try {
-		await client.connect();
-		await client.db("PetInn").collection("PetOwnerListings").insertOne(newPost);
-		return "Posting created succesfully";
-	} catch (e) {
-		console.log(e);
-	} finally {
-		await client.close();
-	}
-}
-
-async function getOwnerListings() {
-	const { MongoClient } = require("mongodb");
-	const uri =
-		"mongodb+srv://tedNorthcoders:PetInn0369@petinn.gyiq5ap.mongodb.net/?retryWrites=true&w=majority";
-	const client = new MongoClient(uri);
-
-	async function findListings() {
-		const cursor = await client
-			.db("PetInn")
-			.collection("PetOwnerListings")
-			.find({});
-		const result = await cursor.toArray();
-
-		// console.log(result);
-		return result;
-	}
-
-	try {
-		await client.connect();
-		const result = await findListings();
-		return result;
-	} finally {
-		setTimeout(() => {
-			client.close();
-		}, 1500);
-	}
-}
-
-async function deleteOwnerListing(id) {
-	const { MongoClient, ObjectId } = require("mongodb");
-	const uri =
-		"mongodb+srv://tedNorthcoders:PetInn0369@petinn.gyiq5ap.mongodb.net/?retryWrites=true&w=majority";
-	const client = new MongoClient(uri);
-
-	async function deleteListing() {
-		const deleteData = { _id: ObjectId(id) };
-		await client
-			.db("PetInn")
-			.collection("PetOwnerListings")
-			.deleteOne(deleteData);
-	}
-
-	try {
-		await client.connect();
-		await deleteListing();
-	} finally {
-		setTimeout(() => {
-			client.close();
-		}, 1500);
-	}
-}
-
-async function getOneOwnerListing(id) {
-	const { MongoClient, ObjectId } = require("mongodb");
-	const uri =
-		"mongodb+srv://tedNorthcoders:PetInn0369@petinn.gyiq5ap.mongodb.net/?retryWrites=true&w=majority";
-	const client = new MongoClient(uri);
-
-	async function findListing() {
-		const findData = { _id: ObjectId(id) };
-		const cursor = await client
-			.db("PetInn")
-			.collection("PetOwnerListings")
-			.findOne(findData);
-		return cursor;
-	}
-
-	try {
-		await client.connect();
-		const result = await findListing();
-		return result;
-	} finally {
-		setTimeout(() => {
-			client.close();
-		}, 1500);
-	}
-}
-
-module.exports = {
-	postOwnerListing,
-	getOwnerListings,
-	deleteOwnerListing,
-	getOneOwnerListing,
+export const getOwnerListings = () => {
+  return axios
+    .get("https://busy-ruby-narwhal-kit.cyclic.app/api/owners/listings")
+    .then(({ data }) => {
+      return data;
+    });
 };
+
+export const postOwnerListing = () => {};
+
+export const deleteOwnerListing = () => {};
+
+export const getOneOwnerListing = () => {};
