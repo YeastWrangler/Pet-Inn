08618e2823e5bcba23d67f3c4e87a21dfdb9080a
marvinobig <88102362+marvinobig@users.noreply.github.com>
2022-09-13 12:55:25 +0100

    remove console.logs

--- dbCalls/User.js
+++ dbCalls/User.js
@@ -9,11 +9,9 @@ export const getUserInfo = (username) => {
 };
 
 export const loginUser = (user) => {
-  console.log(user);
   return axios
     .post(`https://busy-ruby-narwhal-kit.cyclic.app/api/users/login`, user)
     .then(({ data }) => {
-      console.log("logged in now:", data);
       return data;
     });
 };
